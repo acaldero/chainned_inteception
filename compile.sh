c007eac6822dd0071a4a8d8b712788a87e206268
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2021-07-28 13:08:47 +0200

    1.0: initial release

--- compile.sh
+++ compile.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 set -x
 
-#echo "compile i-1 + i-2..."
+echo "compile i-1 + i-2..."
 gcc -Wall -g -shared -fPIC -o i-1.so i-1.c -ldl
 gcc -Wall -g -shared -fPIC -o i-2.so i-2.c -ldl
 
-#echo "compile main..."
+echo "compile main..."
 gcc -Wall -g -o main.o -c main.c
 gcc -Wall -g -o main main.o
 
-#echo "execute main..."
+echo "execute main..."
 LD_PRELOAD="./i-2.so:./i-1.so" ./main
+
+echo "clean all..."
+rm *.o *.so main
 
